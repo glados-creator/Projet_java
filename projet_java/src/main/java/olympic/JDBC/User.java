2756cbd0823dfd61efcf05379990a078309b2d0e
killian.carrat@etu.univ-orleans.fr <killian.carrat@etu.univ-orleans.fr>
2024-06-18 14:24:07 +0200

    Debut jdbc

--- projet_java/src/main/java/olympic/JDBC/User.java
+++ projet_java/src/main/java/olympic/JDBC/User.java
@@ -18,14 +18,8 @@ public class User {
         try {
             st = laConnexion.createStatement();
             ResultSet rs = st.executeQuery("select sport_id from Sport where sport_id ='" + nomSport + "'");
-            if (rs.next()) {
-                return rs.getInt(1);
-            } else {
-                st = laConnexion.createStatement();
-                ResultSet rs2 = st.executeQuery("select max(sport_id) from Sport");
-                rs2.next();
-                return rs2.getInt(1) + 1;
-            }
+            rs.next();
+            return rs.getInt(1);
         } catch (SQLException e) {
             return 0;
         }
@@ -35,14 +29,8 @@ public class User {
         try {
             st = laConnexion.createStatement();
             ResultSet rs = st.executeQuery("select pays_id from Pays where pays_id ='" + nomPays + "'");
-            if (rs.next()) {
-                return rs.getInt(1);
-            } else {
-                st = laConnexion.createStatement();
-                ResultSet rs2 = st.executeQuery("select max(pays_id) from Pays");
-                rs2.next();
-                return rs2.getInt(1) + 1;
-            }
+            rs.next();
+            return rs.getInt(1);
         } catch (SQLException e) {
             return 0;
         }
@@ -52,14 +40,9 @@ public class User {
         try {
             st = laConnexion.createStatement();
             ResultSet rs = st.executeQuery("select equipe_id from Equipe where equipe_id ='" + nomEquipe + "'");
-            if (rs.next()) {
-                return rs.getInt(1);
-            } else {
-                st = laConnexion.createStatement();
-                ResultSet rs2 = st.executeQuery("select max(equipe_id) from Equipe");
-                rs2.next();
-                return rs2.getInt(1) + 1;
-            }
+            rs.next();
+            return rs.getInt(1);
+
         } catch (SQLException e) {
             return 0;
         }
@@ -69,14 +52,9 @@ public class User {
         try {
             st = laConnexion.createStatement();
             ResultSet rs = st.executeQuery("select epreuve_id from Epreuve where epreuve_id ='" + nomEpreuve + "'");
-            if (rs.next()) {
-                return rs.getInt(1);
-            } else {
-                st = laConnexion.createStatement();
-                ResultSet rs2 = st.executeQuery("select max(epreuve_id) from Epreuve");
-                rs2.next();
-                return rs2.getInt(1) + 1;
-            }
+            rs.next();
+            return rs.getInt(1);
+
         } catch (SQLException e) {
             return 0;
         }
@@ -93,36 +71,91 @@ public class User {
         }
     }
 
+    public int getIdSportProchain() throws SQLException {
+        try {
+            st = laConnexion.createStatement();
+            ResultSet rs2 = st.executeQuery("select max(sport_id) from Sport");
+            rs2.next();
+            return rs2.getInt(1) + 1;
+        } catch (SQLException e) {
+            return 0;
+        }
+    }
 
+    public int getIdPaysProchain() throws SQLException {
+        try {
+            st = laConnexion.createStatement();
+            ResultSet rs2 = st.executeQuery("select max(pays_id) from Pays");
+            rs2.next();
+            return rs2.getInt(1) + 1;
+        } catch (SQLException e) {
+            return 0;
+        }
+    }
 
     public void ajouteAthlete(String nom, String prenom, String sexe, String force, String endurance, String agilite,
             String pays, String epreuve) throws SQLException {
-        PreparedStatement ps = this.laConnexion.prepareStatement(
-                "insert into Athlete (athlete_id, nom, prenom, sexe, forceA, enduranceA, enduranceA, pays_id, epreuve_id, equipe_id) values (?,?,?,?,?,?,?,?,?,?)");
+        try {
+            PreparedStatement ps = this.laConnexion.prepareStatement(
+                    "insert into Athlete (athlete_id, nom, prenom, sexe, forceA, enduranceA, enduranceA, pays_id) values (?,?,?,?,?,?,?,?)");
 
-        st = this.laConnexion.createStatement();
-        ResultSet test = st.executeQuery("select collectifs from Epreuve where nom='" + epreuve + "'");
-        test.next();
-        Boolean coll = test.getBoolean("collectifs");
+            st = this.laConnexion.createStatement();
+            ResultSet test = st.executeQuery("select collectifs from Epreuve where nom='" + epreuve + "'");
+            test.next();
+            Boolean coll = test.getBoolean("collectifs");
+            int id = this.getIdAthleteProchain();
+            ps.setInt(1, id);
+            ps.setString(2, nom);
+            ps.setString(3, prenom);
+            ps.setString(4, sexe);
+            ps.setDouble(5, Double.parseDouble(force));
+            ps.setDouble(6, Double.parseDouble(endurance));
+            ps.setDouble(7, Double.parseDouble(agilite));
+            ps.setInt(8, Integer.parseInt(pays));
 
-        ps.setInt(1, this.getIdAthleteProchain());
-        ps.setString(2, nom);
-        ps.setString(3, prenom);
-        ps.setString(4, sexe);
-        ps.setDouble(5, Double.parseDouble(force));
-        ps.setDouble(6, Double.parseDouble(endurance));
-        ps.setDouble(7, Double.parseDouble(agilite));
-        ps.setInt(8, Integer.parseInt(pays));
+            if (coll) {
+                PreparedStatement liason = this.laConnexion
+                        .prepareStatement("insert into APPARTIENT (equipe_id, athlete_id) values (?, ?)");
+                liason.setInt(1, this.getIdEquipe(epreuve));
+                liason.setInt(2, id);
+            } else {
 
-        if (coll) {
-            ps.setNull(9, java.sql.Types.NULL);
-            ps.setInt(10, 0);
-        } else {
-            ps.setInt(9, 0);
-            ps.setNull(10, java.sql.Types.NULL);
+                PreparedStatement liason = this.laConnexion
+                        .prepareStatement("insert into PARTICIPE_ATHLETE (athlete_id, epreuve_id) values (?, ?)");
+                liason.setInt(1, id);
+                liason.setInt(2, this.getIdEpreuve(epreuve));
+            }
+
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println("Erreur d'insertion");
         }
 
-        ps.executeUpdate();
+    }
 
+    public void ajouteSport(String nomSport) throws SQLException {
+        PreparedStatement ps = this.laConnexion.prepareStatement(
+                "insert into Sport (sport_id, nom) values (?,?)");
+        ps.setInt(1, this.getIdSportProchain());
+        ps.setString(2, nomSport);
+        ps.executeUpdate();
+    }
+
+    public void ajoutePays(String nomPays) throws SQLException {
+        PreparedStatement ps = this.laConnexion.prepareStatement(
+                "insert into Pays (pays_id, nom) values (?,?)");
+        ps.setInt(1, this.getIdPaysProchain());
+        ps.setString(2, nomPays);
+        ps.executeUpdate();
+    }
+
+    public void ajouteEquipe(String nomEquipe, String pays, String sport) throws SQLException {
+        PreparedStatement ps = this.laConnexion.prepareStatement(
+                "insert into Equipe (equipe_id, nom, pays_id, sport_id) values (?,?,?,?)");
+        ps.setInt(1, this.getIdEquipe(nomEquipe));
+        ps.setString(2, nomEquipe);
+        ps.setInt(3, this.getIdPays(pays));
+        ps.setInt(4, this.getIdSport(sport));
+        ps.executeUpdate();
     }
 }
--- projet_java/src/main/java/olympic/script.sql
+++ projet_java/src/main/java/olympic/script.sql
@@ -1,70 +1,94 @@
-DROP TABLE Resultat;
+DROP TABLE PARTICIPE_ATHLETE;
+DROP TABLE PARTICIPE_EQUIPE;
 DROP TABLE Epreuve;
 DROP TABLE Athlete;
 DROP TABLE Equipe;
 DROP TABLE Sport;
 DROP TABLE Pays;
 
- 
 
-
--- Table "Pays"
 CREATE TABLE Pays (
     pays_id INT PRIMARY KEY,
     nom_pays VARCHAR(100)
 );
 
--- Table "Sport"
 CREATE TABLE Sport (
     sport_id INT PRIMARY KEY,
     nom_sport VARCHAR(100)
 );
 
-
--- Table "Équipe"
-CREATE TABLE Equipe (
-    equipe_id INT PRIMARY KEY,
-    nom_equipe VARCHAR(100),
-    pays_id INT,
-    sport_id INT,
-    FOREIGN KEY (pays_id) REFERENCES Pays(pays_id),
-    FOREIGN KEY (sport_id) REFERENCES Sport(sport_id)
-);
-
--- Table "Athlète"
-CREATE TABLE Athlete (
-    athlete_id INT PRIMARY KEY,
-    nom VARCHAR(100),
-    prenom VARCHAR(100),
-    sexe VARCHAR(10),
-    forceA INT,
-    enduranceA INT,
-    agiliteA INT,
-    pays_id INT,
-    sport_id INT,
-    equipe_id INT,
-
-    FOREIGN KEY (pays_id) REFERENCES Pays(pays_id),
-    FOREIGN KEY (sport_id) REFERENCES Sport(sport_id),
-    FOREIGN KEY (equipe_id) REFERENCES Equipe(equipe_id)
-);
-
-
--- Table "Épreuve"
 CREATE TABLE Epreuve (
     epreuve_id INT PRIMARY KEY,
     nom_epreuve VARCHAR(100),
-    type VARCHAR(20),
+    collectifs boolean;
     genre VARCHAR(20),
     sport_id INT,
     FOREIGN KEY (sport_id) REFERENCES Sport(sport_id)
 );
 
--- Table "Résultat"
-CREATE TABLE Resultat (
-    resultat_id INT PRIMARY KEY,
+
+CREATE TABLE Equipe (
+    equipe_id INT PRIMARY KEY,
+    nom_equipe VARCHAR(100),
+    pays_id INT,
+    epreuve_id INT,
+    FOREIGN KEY (pays_id) REFERENCES Pays(pays_id),
+    FOREIGN KEY (epreuve_id) REFERENCES Epreuve(epreuve_id)
+);
+
+
+CREATE TABLE Athlete (
+    athlete_id INT PRIMARY KEY,
+    nom VARCHAR(100),
+    prenom VARCHAR(100),
+    sexe VARCHAR(1),
+    forceA INT,
+    enduranceA INT,
+    agiliteA INT,
+    pays_id INT,
+
+    FOREIGN KEY (pays_id) REFERENCES Pays(pays_id),
+    FOREIGN KEY (equipe_id) REFERENCES Equipe(equipe_id)
+);
+
+
+CREATE TABLE PARTICIPE_ATHLETE(
     athlete_id INT,
     epreuve_id INT,
     FOREIGN KEY (athlete_id) REFERENCES Athlete(athlete_id),
     FOREIGN KEY (epreuve_id) REFERENCES Epreuve(epreuve_id)
-);
+)
+
+CREATE TABLE PARTICIPE_EQUIPE(
+    equipe_id INT,
+    epreuve_id INT,
+    FOREIGN KEY (equipe_id) REFERENCES Equipe(equipe_id),
+    FOREIGN KEY (epreuve_id) REFERENCES Epreuve(epreuve_id)
+)
+
+CREATE TABLE APPARTIENT(
+    equipe_id INT,
+    athlete_id INT,
+    FOREIGN KEY (equipe_id) REFERENCES Equipe(equipe_id),
+    FOREIGN KEY (athlete_id) REFERENCES Athlete(athlete_id)
+)
+
+
+
+----------------------------------------------------------------
+
+--Creation des tables de connexion
+
+CREATE TABLE Role (
+    role_id INT PRIMARY KEY,
+    nom_role VARCHAR(100)
+)
+
+CREATE TABLE Utilisateur (
+    utilisateur_id INT PRIMARY KEY,
+    nom VARCHAR(100),
+    prenom VARCHAR(100),
+    password VARCHAR(100),
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES Role(role_id)
+)
